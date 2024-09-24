c6bb00fc4429e9e3767f3ec719e4367b8776cbcc
Bivek Gharti <gcbibek3353@gmail.com>
2024-09-24 21:51:12 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import { roboto,sofadi,playpen } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+        <body className={`${playpen.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Roboto,Sofadi_One,Playpen_Sans } from 'next/font/google';
+
+ export const roboto = Roboto({
+    subsets : ['latin'],
+    weight : '400'
+ });
+
+ export const sofadi = Sofadi_One({
+    subsets : ['latin'],
+    weight : '400'
+ })
+ export const playpen = Playpen_Sans({
+    subsets : ['latin'],
+    weight : '400'
+ })
